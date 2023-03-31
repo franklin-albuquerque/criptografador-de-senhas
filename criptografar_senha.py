d934285f2036e2bc5820aeaade43a4de7d9b1d87
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-03-31 03:14:38 -0300

    Realizando pequenas melhorias no código

--- criptografar_senha.py
+++ criptografar_senha.py
@@ -1,15 +1,14 @@
 from bcrypt import hashpw, gensalt
 
 def criptografar(senha):
-    bytes = senha.encode('utf-8')
-    vetor_binario = ''.join(f'{caractere:b}' for caractere in bytes).encode('utf-8')
-    resultado = hashpw(vetor_binario, gensalt(13))
-    return resultado
+    vetor_binario = ''.join(map(bin, bytearray(senha, 'utf-8')))
+    resultado = hashpw(vetor_binario.encode('utf8'), gensalt(13))
+    return resultado.decode('utf-8')
 
 def main():
-    senha = input('Digite sua senha: ')
-    hash = criptografar(senha)
-    print(f'A senha criptografada é: {hash}')
+    senha = input('Digite a senha que deseja criptografar: ')
+    resultado = criptografar(senha)
+    print(f'Sua senha foi criptografada como: {resultado}')
 
 if __name__ == '__main__':
     main()
