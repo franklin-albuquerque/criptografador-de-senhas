26ed11514ff34a6d7997d5bfe6b2fbde4788da12
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-03-31 02:55:27 -0300

    Separando o código em funções

--- criptografar_senha.py
+++ criptografar_senha.py
@@ -1,10 +1,15 @@
 from bcrypt import hashpw, gensalt
 
-senha = input('Digite sua senha: ')
-bytes = senha.encode('utf-8')
-vetor_binario = ''.join(f'{caractere:b}' for caractere in bytes).encode('utf8')
+def criptografar(senha):
+    bytes = senha.encode('utf-8')
+    vetor_binario = ''.join(f'{caractere:b}' for caractere in bytes).encode('utf-8')
+    resultado = hashpw(vetor_binario, gensalt(13))
+    return resultado
 
-print('Criptografando...')
+def main():
+    senha = input('Digite sua senha: ')
+    hash = criptografar(senha)
+    print(f'A senha criptografada é: {hash}')
 
-hash = hashpw(vetor_binario, gensalt(13))
-print(f'A senha criptografada é: {hash}')
+if __name__ == '__main__':
+    main()
