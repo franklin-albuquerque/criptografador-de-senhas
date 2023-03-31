986e296c161a5bf87aacc8dfd4cc8875777028c3
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-03-31 02:47:44 -0300

    Adicionando um vetor binário

--- criptografar_senha.py
+++ criptografar_senha.py
@@ -2,8 +2,9 @@ from bcrypt import hashpw, gensalt
 
 senha = input('Digite sua senha: ')
 bytes = senha.encode('utf-8')
+vetor_binario = ''.join(f'{caractere:b}' for caractere in bytes).encode('utf8')
 
 print('Criptografando...')
 
-hash = hashpw(bytes, gensalt(13))
+hash = hashpw(vetor_binario, gensalt(13))
 print(f'A senha criptografada é: {hash}')
